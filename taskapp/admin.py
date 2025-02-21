3e1735018805f2bcbb458a7ffd3b90fcaa281ab1
Alida Thomas <alidathomas@Alidas-MacBook-Air.local>
2025-02-21 15:58:39 +0530

    Intial commit

--- taskapp/admin.py
+++ taskapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserGroup,Employee
 
+admin.site.register(UserGroup)
+admin.site.register(Employee)
 # Register your models here.
--- taskapp/models.py
+++ taskapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class  UserGroup(models.Model):
+    group_name = models.CharField(max_length=255, unique=True)
+    description = models.CharField(blank=True, null=True)
+
+    def __str__(self):
+        return self.group_name
+    
+
+class  Employee(models.Model):
+    group = models.ForeignKey(UserGroup, on_delete=models.CASCADE, related_name="employees")
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return f"{self.name} ({self.code})"
